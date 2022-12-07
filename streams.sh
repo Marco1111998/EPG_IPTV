608a55c1903631f0ec9d9fadc4bf20c92dc668c0
Marco1111998 <96786575+Marco1111998@users.noreply.github.com>
2022-12-07 16:45:10 +0000

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -2,24 +2,15 @@
 
 # TVI - update the stream URL of TVI
 
-sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
-
-# TVII - update the stream URL of TVII
-
 sed -i "/live_tvi\/live_tvi/ c https://video-auth1.iol.pt/live_tvi/live_tvi/edge_servers/tvi-720_passthrough/chunks.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
-
-# CNN Portugal - update the stream URL of CNN Portugal
-
-sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
 # CNN PORTUGAL - update the stream URL of CNN PORTUGAL
 
 sed -i "/live_cnn/ c https://video-auth1.iol.pt/live_cnn/live_cnn/edge_servers/cnn-720p/chunks.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
-
 # TVI FICCAO - update the stream URL of TVI FICCAO
 
-sed -i "/live_tvi_ficcao/ c https://video-auth1.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/chunks.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
+sed -i "/live_tvi_ficcao/ c https://video-auth1.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720_passthrough/chunks.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
 # TVI AFRICA - update the stream URL of TVI AFRICA
 
@@ -29,11 +20,6 @@ sed -i "/live_tvi_africa/ c https://video-auth1.iol.pt/live_tvi_africa/live_tvi_
 
 sed -i "/live_tvi_reality/ c https://video-auth1.iol.pt/live_tvi_reality/live_tvi_reality/edge_servers/tvireality-480p/chunks.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
-
-
-# TVI Internacional - update the stream URL of TVI Internacional
-
-sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
 # TVI INTERNACIONAL - update the stream URL of TVI INTERNACIONAL
 
